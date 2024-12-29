cf7d2633dbde6acd4ec0bfdf8c66904c245ff7d3
Egor Kostan <20955183+ikostan@users.noreply.github.com>
2024-12-29 10:51:26 -0800

    Update script_player_orange.gd

--- intro_to_godot_by_zenva/Scripts/script_player_orange.gd
+++ intro_to_godot_by_zenva/Scripts/script_player_orange.gd
@@ -2,15 +2,15 @@ extends Sprite2D
 
 var score: int = 0
 var speed: float = 0.0
+var money: float = 10.0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print('score: {0}, speed: {1}'.format({'0':score, '1':speed}))
-
+	global_position.y = 0
+	global_position.x = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	score += 1
-	speed += 0.5
-	print('score: {0}, speed: {1}'.format({'0':score, '1':speed}))
+	global_position.y += 1
+	global_position.x += 1
